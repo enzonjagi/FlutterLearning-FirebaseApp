9c7c5e06c312db11096d3619afee537c680d805a
enzonjagi <amosndungo@gmail.com>
2020-08-22 12:15:11 +0300

    Basic app structure set up

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:WithFirebase/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    //TODO return either home or authenticate
+    return  Home();  
+    
+  }
+}
